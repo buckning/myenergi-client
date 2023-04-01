07b02522be5291f2e8e1a0090661a12db6011121
buckning <andrewmcglynn86@hotmail.com>
2023-04-01 10:25:36 +0100

    Added intent for checking if the car is plugged in or not and the charge rate (#9)
    * Added intent for checking if the car is plugged in or not and the charge rate

---
+++ src/main/java/com/amcglynn/myenergi/aws/intenthandlers/GetPlugStatusIntentHandler.java
@@ -0,0 +1,52 @@
+package com.amcglynn.myenergi.aws.intenthandlers;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.model.services.directive.Header;
+import com.amazon.ask.model.services.directive.SendDirectiveRequest;
+import com.amazon.ask.model.services.directive.SpeakDirective;
+import com.amazon.ask.request.RequestHelper;
+import com.amcglynn.myenergi.EvStatusSummary;
+import com.amcglynn.myenergi.aws.MyZappi;
+import com.amcglynn.myenergi.aws.exception.InvalidDateException;
+import com.amcglynn.myenergi.aws.responses.ZappiDaySummaryCardResponse;
+import com.amcglynn.myenergi.aws.responses.ZappiDaySummaryVoiceResponse;
+import com.amcglynn.myenergi.aws.responses.ZappiEvConnectionStatusCardResponse;
+import com.amcglynn.myenergi.aws.responses.ZappiEvConnectionStatusVoiceResponse;
+import com.amcglynn.myenergi.aws.responses.ZappiMonthSummaryCardResponse;
+import com.amcglynn.myenergi.aws.responses.ZappiMonthSummaryVoiceResponse;
+import com.amcglynn.myenergi.service.ZappiService;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class GetPlugStatusIntentHandler implements RequestHandler {
+
+    private final ZappiService zappiService;
+
+    public GetPlugStatusIntentHandler(ZappiService zappiService) {
+        this.zappiService = zappiService;
+    }
+
+    @Override
+    public boolean canHandle(HandlerInput handlerInput) {
+        return handlerInput.matches(intentName("GetPlugStatus"));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput handlerInput) {
+        var summary = new EvStatusSummary(zappiService.getStatusSummary().get(0));
+
+        return handlerInput.getResponseBuilder()
+                .withSpeech(new ZappiEvConnectionStatusVoiceResponse(summary).toString())
+                .withSimpleCard(MyZappi.TITLE,
+                        new ZappiEvConnectionStatusCardResponse(summary).toString())
+                .build();
+    }
+}
